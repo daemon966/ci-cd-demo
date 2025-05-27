45e57c51d82415bc151a9dd2f0278515caf537c4
Kunal K Dulbaji <121796405+daemon966@users.noreply.github.com>
2025-05-27 13:44:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello CI/CD!'));
+app.get('/', (req, res) => res.send('Hello CI/CD!Its working fine:::::::::::::::::::'));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
